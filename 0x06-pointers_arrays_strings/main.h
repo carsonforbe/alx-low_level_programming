0c0203d46d0bf239e9f750322606f054e5dac9e8
carsonforbe <carsonforbe@gmail.com>
2022-09-24 12:02:16 +0300

    "update"

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,4 +1,16 @@
-#include <string.h>
 #include <stdio.h>
+#include <string.h>
+int my_func() {
+   char str1[100] = "This is ", str2[] = "programiz.com";
 
-char *_strcat(char *dest, char *src);
+   /* concatenates str1 and str2
+    *the resultant string is stored in str1.
+    */
+   strcat(str1, str2);
+
+   puts(str1);
+   
+
+   return 0;
+}
+
