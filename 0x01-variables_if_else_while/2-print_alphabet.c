de621f3e63f41672a5a06e3adef765e767f8e1be
shadow <hackerskull@ghost.Charli>
2025-06-08 13:42:49 -0400

    print alphbet characters using putchar

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - main entry point
+ * 
+ * Return: returns 0 on success
+ */
+int main()
+{
+    char c = 'a';
+
+    for (; c <= 'z'; c++)
+    {
+        putchar(c);
+    }
+    
+    return (0);
+}
