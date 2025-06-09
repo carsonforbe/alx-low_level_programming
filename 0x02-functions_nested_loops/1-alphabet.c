6df49c8359932d2b05da494adaa34df634e16da3
shadow <hackerskull@ghost.Charli>
2025-06-09 08:21:45 -0400

    fixed bug multiple main defition

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -19,15 +19,3 @@ void print_alphabet(void)
     putchar('\n');
     
 }
-
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-    print_alphabet();
-
-    return (0);
-}
