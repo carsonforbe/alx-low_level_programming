a8cd59050e45929dce866f486bd450668db2eb43
shadow <hackerskull@ghost.Charli>
2025-06-09 02:41:50 -0400

    print base16 alphabet

---
+++ 0x01-variables_if_else_while/print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    int i;
+    char ch;
+
+    for (i = 0; i <= 10; i++)
+    {
+        printf("%d", i);
+    }
+    for (ch = 'a'; ch <= 'f'; ch++)
+    {
+        printf("%c", ch);
+    }
+    return (0);
+}
