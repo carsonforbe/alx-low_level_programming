75f36023000ad07a88d435896a2b18cb4b9676fd
shadow <hackerskull@ghost.Charli>
2025-06-09 02:18:30 -0400

    fixed a bug

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,13 +9,12 @@ int main(void)
 {
     char ch;
 
-    for (ch = 'a'; ch <= 'Z'; ch++)
+    for (ch = 'a'; ch <= 'z'; ch++)
     {
-        if (ch != 'e' && ch != 'q')
+    if (ch != 'e' && ch != 'q')
         {
             putchar(ch);
-        }
-        
+        }    
     }
     putchar('\n');
 
